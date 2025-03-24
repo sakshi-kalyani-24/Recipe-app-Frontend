cd34d217a2ae7dff9d575fd6a0f6afdcd02c1ed6
sakshi-kalyani-24 <sakshikalyani157@gmail.com>
2025-03-24 15:51:52 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Navbar from './component/Navbar'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import { BrowserRouter as Router , Route, Routes, Navigate } from "react-router-dom";
+import './App.css'
+
+const App = () => {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        /* Redirect to login if not logged in * /
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<Home />} />
+        
+      </Routes>
+    </Router>
+    
+    
+    
+      
+  )
+}
+
+export default App
